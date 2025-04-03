3868ca078579253724518df7c9ae3293612cdcbe
Angel Montemayor Davila <204801005+A01785840-AMD@users.noreply.github.com>
2025-04-03 13:56:52 -0600

    Update base path

--- vite.config.js
+++ vite.config.js
@@ -5,7 +5,7 @@ import path from 'path'
 export default defineConfig(({ mode }) => {
     const env = loadEnv(mode, process.cwd());
     return {
-        base: `/`,
+        base: `/angelmontemayordavila/`,
         resolve: {
             alias: {
                 '@': path.resolve(__dirname, 'src'),
